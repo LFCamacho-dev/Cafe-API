86710b044ab34ef3f78615024e6d4aa487ab916f
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-02-21 19:52:26 -0600

    created a few routes to interact with the database, jsonify-ing the results returning json instead of html. Added a new row in database through Postman making a POST request.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from random import choice
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -23,7 +25,11 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
-#region CRUD COMMANDS
+    def turn_to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+# region CRUD COMMANDS
 # CREATE RECORD
 # new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
 # db.session.add(new_book)
@@ -51,22 +57,86 @@ class Cafe(db.Model):
 # book_to_delete = Book.query.get(book_id)
 # db.session.delete(book_to_delete)
 # db.session.commit()
-#endregion
+# endregion
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
-## HTTP GET - Read Record
 
-## HTTP POST - Create Record
+# HTTP GET - Read Record
+@app.route("/random")
+def random():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = choice(cafes)
 
-## HTTP PUT/PATCH - Update Record
+    return jsonify(cafe=random_cafe.turn_to_dict())
+    # return jsonify(cafe={
+    #     # Omit the id from the response
+    #     # "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #
+    #     # Put some properties in a sub-category
+    #     "amenities": {
+    #         "seats": random_cafe.seats,
+    #         "has_toilet": random_cafe.has_toilet,
+    #         "has_wifi": random_cafe.has_wifi,
+    #         "has_sockets": random_cafe.has_sockets,
+    #         "can_take_calls": random_cafe.can_take_calls,
+    #         "coffee_price": random_cafe.coffee_price,
+    #     }
+    # })
 
-## HTTP DELETE - Delete Record
 
+@app.route('/all')
+def all_cafes():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.turn_to_dict() for cafe in cafes])
+
+
+@app.route('/search')
+def search():
+    location = request.args.get('loc')
+    cafes = db.session.query(Cafe).filter_by(location=location).all()
+    if cafes:
+        return jsonify(cafes=[cafe.turn_to_dict() for cafe in cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
+# HTTP POST - Create Record
+
+def make_bool(val: int) -> bool:
+    return bool(int(val))
+
+
+@app.route('/add', methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get('name'),
+        map_url=request.form.get('map_url'),
+        img_url=request.form.get('img_url'),
+        location=request.form.get('location'),
+        seats=request.form.get('seats'),
+        has_toilet=request.form.get('has_toilet', type=bool),
+        has_wifi=request.form.get('has_wifi', type=bool),
+        has_sockets=request.form.get('has_sockets', type=bool),
+        can_take_calls=request.form.get('can_take_calls', type=bool),
+        coffee_price=request.form.get('coffee_price'),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 
 if __name__ == '__main__':
     app.run(debug=True)
